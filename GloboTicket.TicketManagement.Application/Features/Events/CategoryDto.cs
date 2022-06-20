e78af631389cefa0b6d1c587d75c649693e4052c
zigzacvy <lethanhphuong17051989@gmail.com>
2022-06-21 00:29:23 +0700

    Add any logic about event handler

---
+++ GloboTicket.TicketManagement.Application/Features/Events/CategoryDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
