b5818118809104dd28e673d55baa16f09cee3582
zigzacvy <lethanhphuong17051989@gmail.com>
2022-06-22 23:31:01 +0700

    Add Infrastructure

--- GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.VisualBasic;
 
 namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
 {
     public interface IOrderRepository : IAsyncRepository<Order>
     {
-        
+        Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size);
+
+        Task<int> GetTotalCountOfOrdersForMonth(DateTime date);
+
     }
 }
--- GloboTicket.TicketManagement.Domain/Entities/Event.cs
+++ GloboTicket.TicketManagement.Domain/Entities/Event.cs
@@ -9,6 +9,7 @@ namespace GloboTicket.TicketManagement.Domain.Entities
         public Guid EventId { get; set; }
         public string Name { get; set; }
         public string Artist { get; set; }
+        public int Price { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string  ImageUrl { get; set; }
